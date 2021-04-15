3afbbb66c2045ab1a539e65dd4adaf8596f96534
Marc Wright <marc@generalassemb.ly>
2021-04-15 11:36:12 -0500

    login working, save jwt in local storage

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -8,8 +8,7 @@ const herokuUrl = 'https://damp-bayou-38809.herokuapp.com';
 })
 export class UserService {
 
-
-  constructor(private http: HttpClient) { console.log('user service loaded'); }
+  constructor(private http: HttpClient) { }
 
   registerUser(newUser): void {
     console.log(newUser);
@@ -17,5 +16,22 @@ export class UserService {
       .post(`${herokuUrl}/auth/users/register`, newUser)
       .toPromise()
       .then(response => console.log(response));
+  }
+
+  loginUser(user): void {
+    console.log(user);
+    this.http
+      .post(`${herokuUrl}/auth/users/login`, user)
+      .toPromise()
+      .then(response => {
+        const token = response['jwt'];
+        localStorage.setItem('currentUser', `${user.email}`);
+        localStorage.setItem('token', `${token}`);
+        console.log(response, token);
+      })
+      .catch(error => console.log(error));
+      // .then(() => {
+      //   this.getCategories();
+      // });
   }
 }
