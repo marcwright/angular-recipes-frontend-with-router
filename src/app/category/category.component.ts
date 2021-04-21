2fe3b2e97974f6705ebf9f2faf37ed7e8bff7d52
Marc Wright <marc@generalassemb.ly>
2021-04-21 14:34:30 -0500

    add extra fields to recipe form

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -10,13 +10,25 @@ import { ActivatedRoute } from '@angular/router';
 export class CategoryComponent implements OnInit {
   categoryId: string;
   category: any;
-  recipeName = '';
+  recipeName:string;
+  recipeTime: string;
+  recipePortions:string;
+  recipeIngredients:string;
+  recipeSteps:string;
+  recipeIsPublic:boolean;
 
   constructor(private route: ActivatedRoute, private categoryService: CategoryService) { }
 
   createRecipe(): any {
     console.log('component: ', this.category, this.recipeName);
-    const newRecipe = {name: this.recipeName};
+    const newRecipe = {
+      name: this.recipeName,
+      time: this.recipeTime,
+      portions: this.recipePortions,
+      ingredients: this.recipeIngredients,
+      steps: this.recipeSteps,
+      isPublic: this.recipeIsPublic
+    };
     this.categoryService.createRecipe(this.category, newRecipe).subscribe(response => {
       this.category.recipeList = [...this.category.recipeList, response];
       console.log(response);
